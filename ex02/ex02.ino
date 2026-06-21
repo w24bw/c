839915a3e2a239650d8ac60fbdba3312392dedcd
w24bw <27877929494@qq.com>
2026-06-21 19:13:17 +0800

    feat(ex02): 添加 millis() 实现 1Hz LED 闪烁

--- ex02/ex02.ino
+++ ex02/ex02.ino
@@ -1,7 +1,7 @@
 // ex02.ino - 使用 millis() 实现 1Hz LED 闪烁
 const int ledPin = 2;
 unsigned long previousMillis = 0;
-const long interval = 500;  // 500ms 亮 + 500ms 灭 = 1Hz
+const long interval = 500;
 
 void setup() {
   Serial.begin(115200);
@@ -10,7 +10,6 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
-  
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     digitalWrite(ledPin, !digitalRead(ledPin));
